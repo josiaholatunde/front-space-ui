189626dbb42ff9be3ac351944bcce0e8c78a10a6
Ogunboyejo Olatunde <tunde.ogunboyejo@xputertechnologies.com>
2019-04-02 17:52:25 +0100

    finished a personal ui web design project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if (window.scrollY >= 150) {
+    let elem = document.getElementById('mainHeader');
+    elem.style.opacity = 0.4;
+}
+
+/* document.getElementById('hamburger').addEventListener('click', () => {
+    var list = document.getElementsByClassName('nav-item');
+    for (let index = 0; index < list.length; index++) {
+        list[index].style.display = 'block';   
+    }
+}); */
